a743477fddd2ddc9e668280fbe52886c6a14cddc
manitofigh <manitofigh@protonmail.com>
2024-07-17 18:28:23 -0400

    Leader+D for :%d

--- init.lua
+++ init.lua
@@ -78,6 +78,7 @@ vim.keymap.set("v", "<leader>p", '"_dP', { noremap = true }) -- replace what is 
 vim.keymap.set("n", "<leader>j", ":cnext<CR>", { noremap = true, silent = true, desc = "Next quickfix item" })
 vim.keymap.set("n", "<leader>k", ":cprev<CR>", { noremap = true, silent = true, desc = "Previous quickfix item" })
 vim.keymap.set("n", "<leader>st", ":NvimTreeToggle<CR>", { desc = "[S]idebar [T]oggle" })
+vim.keymap.set("n", "<leader>D", ":%d<CR>", { noremap = true, silent = true, desc = "[D]elete whole content" })
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
@@ -440,7 +441,7 @@ require("lazy").setup({
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					-- map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
