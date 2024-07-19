ff5fa126f14a74384ab491332c2a98a366e57f3c
manitofigh <manitofigh@protonmail.com>
2024-07-18 23:00:03 -0400

    Bug line 3 remove init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.g.maplocalleader =  cs
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
